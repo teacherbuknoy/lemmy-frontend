d8babbba1fd9e5f4dfd736234a27b6076aec915f
Francis Rubio <francis.rubio@dice205.com>
2023-06-12 16:20:06 +0800

    Update script compiler

--- src/assets/scripts/__scripts.11ty.js
+++ src/assets/scripts/__scripts.11ty.js
@@ -1,14 +1,42 @@
-const babel = require('@babel/core')
+const fs = require('fs')
 const path = require('path')
+const webpack = require('webpack')
+const { fs: mfs } = require('memfs')
+const isProd = process.env.ELEVENTY_ENV === 'production'
+
+require('dotenv').config()
 
 class Script {
   constructor() {
     this.inputFiles = {
-      index: 'index.js'
+      index: 'index.js',
     }
   }
 
   data() {
+    const outputPath = path.resolve(__dirname, "../../memory-fs/js/")
+    const envPlugin = new webpack.EnvironmentPlugin({ ELEVENTY_ENV: process.env.ELEVENTY_ENV })
+    const vars = new webpack.DefinePlugin({
+      GENERATED: JSON.stringify(new Date().toISOString()),
+    })
+
+    const rules = [
+      {
+        test: /\.m?js$'/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+
+    const webpackEntry = {}
+    Object.keys(this.inputFiles).forEach(key => webpackEntry[key] = `./src/assets/scripts/${this.inputFiles[key]}`)
+
     return {
       eleventyExcludeFromCollections: true,
       entryPoints: this.inputFiles,
@@ -18,19 +46,67 @@ class Script {
         size: 1
       },
       permalink: ({ bundleName }) => `/assets/scripts/${bundleName}.js`,
+      webpackConfig: {
+        mode: isProd ? 'production' : 'development',
+        entry: webpackEntry,
+        output: { path: outputPath },
+        module: { rules },
+        plugins: [envPlugin, vars],
+        target: 'web',
+        devtool: 'source-map'
+      }
     }
   }
-  
-  compile(bundleName) {
-    console.log('[JS] Compiling: ', bundleName)
-    const filepath = path.join(__dirname, this.inputFiles[bundleName])
-    const transformedCode = babel.transformFileSync(filepath, {})
 
-    return `${transformedCode.code}\n//# sourceMappingURL=${bundleName}.js.map`
+  compile(bundleName, webpackConfig) {
+    const compiler = webpack(webpackConfig)
+    compiler.outputFileSystem = mfs
+    compiler.inputFileSystem = fs
+    compiler.intermediateFileSystem = mfs
+
+    return new Promise((resolve, reject) => {
+      compiler.run((err, stats) => {
+        if (err || stats.hasErrors()) {
+          const errors = err || (stats.compilation ? stats.compilation.errors : null)
+          reject(errors)
+          return
+        }
+
+        const outputFile = `${webpackConfig.output.path}/${bundleName}.js`
+        const outputSourceMap = `${webpackConfig.output.path}/${bundleName}.js.map`
+        const charset = 'utf-8'
+
+        console.log('[Webpack] Compiling sourcemap:', this.inputFiles[bundleName])
+        // mfs.readFile(outputSourceMap, charset, (err, data) => {
+        //   if (err) console.error("[WEBPACK] Sourcemap:", err)
+        //   else console.log("[WEBPACK] Sourcemap:", data)
+        // })
+
+        mfs.readdir(webpackConfig.output.path, (err, files) => {
+          if (err) console.error("[WEBPACK] Sourcemap:", err)
+          else {
+            files.forEach(file => console.log("[WEBPACK] memfs file:", file))
+          }
+        })
+
+        mfs.readFile(outputFile, charset, (err, data) => {
+          if (err) reject(err)
+          else resolve(data)
+        })
+      })
+    })
+
+    // return `${transformedCode.code}\n//# sourceMappingURL=${bundleName}.js.map`
   }
-  
-  render({ bundleName }) {
-    return this.compile(bundleName)
+
+  async render({ bundleName, webpackConfig }) {
+    try {
+      const result = await this.compile(bundleName, webpackConfig)
+      return result
+    } catch (err) {
+      console.error(err)
+      return "// Bundle has errors"
+    }
   }
 }
 
--- src/assets/scripts/__sourcemap.11ty.js
+++ src/assets/scripts/__sourcemap.11ty.js
@@ -1,14 +1,39 @@
-const babel = require('@babel/core')
+const fs = require('fs')
 const path = require('path')
-
+const webpack = require('webpack')
+const { fs: mfs } = require('memfs')
+const isProd = process.env.ELEVENTY_ENV === 'production'
 class ScriptSourceMap {
   constructor() {
     this.inputFiles = {
-      index: 'index.js'
+      index: 'index.js',
     }
   }
 
   data() {
+    const outputPath = path.resolve(__dirname, "../../memory-fs/js/")
+    const envPlugin = new webpack.EnvironmentPlugin({ ELEVENTY_ENV: process.env.ELEVENTY_ENV })
+    const vars = new webpack.DefinePlugin({
+      GENERATED: JSON.stringify(new Date().toISOString())
+    })
+
+    const rules = [
+      {
+        test: /\.m?js$'/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+
+    const webpackEntry = {}
+    Object.keys(this.inputFiles).forEach(key => webpackEntry[key] = `./src/assets/scripts/${this.inputFiles[key]}`)
+
     return {
       eleventyExcludeFromCollections: true,
       entryPoints: this.inputFiles,
@@ -18,19 +43,52 @@ class ScriptSourceMap {
         size: 1
       },
       permalink: ({ bundleName }) => `/assets/scripts/${bundleName}.js.map`,
+      webpackConfig: {
+        mode: isProd ? 'production' : 'development',
+        entry: webpackEntry,
+        output: { path: outputPath },
+        module: { rules },
+        plugins: [envPlugin, vars],
+        target: 'web',
+        devtool: 'source-map'
+      }
     }
   }
-  
-  compile(bundleName) {
-    console.log('[JS] Compiling: ', bundleName)
-    const filepath = path.join(__dirname, this.inputFiles[bundleName])
-    const transformedCode = babel.transformFileSync(filepath, {})
 
-    return JSON.stringify(transformedCode.map)
+  compile(bundleName, webpackConfig) {
+    const compiler = webpack(webpackConfig)
+    compiler.outputFileSystem = mfs
+    compiler.inputFileSystem = fs
+    compiler.intermediateFileSystem = mfs
+
+    return new Promise((resolve, reject) => {
+      compiler.run((err, stats) => {
+        if (err || stats.hasErrors()) {
+          const errors = err || (stats.compilation ? stats.compilation.errors : null)
+          reject(errors)
+          return
+        }
+
+        const outputSourceMap = `${webpackConfig.output.path}/${bundleName}.js.map`
+        const charset = 'utf-8'
+        mfs.readFile(outputSourceMap, charset, (err, data) => {
+          if (err) reject(err)
+          else resolve(data)
+        })
+      })
+    })
+
+    // return `${transformedCode.code}\n//# sourceMappingURL=${bundleName}.js.map`
   }
-  
-  render({ bundleName }) {
-    return this.compile(bundleName)
+
+  async render({ bundleName, webpackConfig }) {
+    try {
+      const result = await this.compile(bundleName, webpackConfig)
+      return result
+    } catch (err) {
+      console.error(err)
+      return "// Bundle has errors"
+    }
   }
 }
 
